ad6c41cd76e6b48db92671212c5c3db69494b17c
satueldev <152908027+satueldev@users.noreply.github.com>
2023-12-24 20:57:24 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,9 @@
+/*let username = document.getElementById('text')
+let names = 'samuel'
+let welcome = document.getElementById('welcome')
+let para = document.getElementById('para')
+function getname(){
+    console.log(document.getElementById('para').innerText = text.value)  
+   document.getElementById('welcome').innerText = 'Welcome' + ' ' + text.value 
+
+}*/
---
+++ level5.js
@@ -0,0 +1,23 @@
+let name = document.getElementById('names')
+let answer = Math.round((Math.random() * 250))
+let score = 0;
+let nextbtn = document.getElementById('next')
+let correct = document.getElementById('correct')
+let wrong= document.getElementById('wrong')
+function getinput(){
+   console.log(Number(names.value));
+     console.log(answer)
+ 
+    if(names.value==answer){
+      document.getElementById('score').innerText = score+=10;
+     console.log(  document.getElementById('score').innerText = score)
+     document.getElementById('correct').style.opacity = '1';
+      document.getElementById('next').style.opacity = '1';
+      document.getElementById('wrong').style.opacity = '0'
+    }
+    else{
+      document.getElementById('wrong').style.opacity = '1'
+      document.getElementById('correct').style.opacity = '0'
+      document.getElementById('next').style.opacity = '0'
+    }
+  }
